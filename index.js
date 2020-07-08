8421b8b798dd72722ecb808e0e981d5693b1ed09
Chester <chesterdeveloper2000@gmail.com>
2020-07-08 11:49:32 +0800

    updated horizontal wall values

--- index.js
+++ index.js
@@ -91,6 +91,10 @@ const stepThroughCell = (row, column) => {
             verticals[row][column - 1] = true
         } else if (direction === "right") {
             verticals[row][column] = true
+        } else if (direction === "up") {
+            horizontals[row -1][column] = true
+        } else if (direction === "down") {
+            horizontals[row][column] = true
         }
 
     }
@@ -99,4 +103,4 @@ const stepThroughCell = (row, column) => {
     // visit that next cell
 }
 
-stepThroughCell(1, 1)
+stepThroughCell(startRow, startColumn)
